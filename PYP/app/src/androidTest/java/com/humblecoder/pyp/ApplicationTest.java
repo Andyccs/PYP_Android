a6fcb99217be8e45d9bcbf4e3553ff3736f9809c
andy ccs <andychong2@gmail.com>
2014-10-23 21:12:13 +0800

    add travis yml

--- PYP/app/src/androidTest/java/com/humblecoder/pyp/ApplicationTest.java
+++ PYP/app/src/androidTest/java/com/humblecoder/pyp/ApplicationTest.java
@@ -10,4 +10,18 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
     }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testPrecondition(){
+
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
 }
